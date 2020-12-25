022543d64329b6c86403d52f005fb49b005cf222
Admin <grasth@users.noreply.github.com>
2020-12-25 14:27:48 +0400

    feature

--- src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
+++ src/main/java/com/epam/izh/rd/online/service/StreamApiTextStatisticsAnalyzer.java
@@ -2,10 +2,9 @@ package com.epam.izh.rd.online.service;
 
 import com.epam.izh.rd.online.helper.Direction;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.Collections.*;
 
@@ -14,40 +13,47 @@ import static java.util.Collections.*;
  * {@link SimpleTextStatisticsAnalyzer}.
  */
 public class StreamApiTextStatisticsAnalyzer implements TextStatisticsAnalyzer {
-    TextStatisticsAnalyzer analyzer = new SimpleTextStatisticsAnalyzer();
-
     @Override
     public int countSumLengthOfWords(String text) {
-        return analyzer.countSumLengthOfWords(text);
+        return getWords(text).stream().mapToInt(String::length).sum();
     }
 
     @Override
     public int countNumberOfWords(String text) {
-        return analyzer.countNumberOfWords(text);
+        return (int) getWords(text).stream().count();
     }
 
     @Override
     public int countNumberOfUniqueWords(String text) {
-        return analyzer.countNumberOfUniqueWords(text);
+        return (int) getUniqueWords(text).stream().count();
     }
 
     @Override
     public List<String> getWords(String text) {
-        return analyzer.getWords(text);
+        ArrayList<String> words = new ArrayList<>();
+        Arrays.stream(text.split("\\W+")).forEach(words::add);
+        return words;
     }
 
     @Override
     public Set<String> getUniqueWords(String text) {
-        return analyzer.getUniqueWords(text);
+        return getWords(text).stream().collect(Collectors.toSet());
     }
 
     @Override
     public Map<String, Integer> countNumberOfWordsRepetitions(String text) {
-        return analyzer.countNumberOfWordsRepetitions(text);
+        return getUniqueWords(text).stream().collect(Collectors.toMap(e -> e,
+                e -> Collections.frequency(getWords(text), e)));
     }
 
     @Override
     public List<String> sortWordsByLength(String text, Direction direction) {
-        return analyzer.sortWordsByLength(text, direction);
+        return getWords(text).stream().sorted
+                (
+                        (str1, str2) -> direction.equals(Direction.ASC) ?
+                                str1.length() - str2.length() :
+                                str2.length() - str1.length()
+                )
+                .collect(Collectors.toList());
     }
 }
